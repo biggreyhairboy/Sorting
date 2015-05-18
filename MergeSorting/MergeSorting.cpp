5762694972da0887f4a3af5c3eb8b0130090c368
biggreyhairboy <biggreyhairboy@gmail.com>
2015-05-18 16:48:50 +0800

    添加数组copy的功能

--- MergeSorting/MergeSorting.cpp
+++ MergeSorting/MergeSorting.cpp
@@ -7,7 +7,7 @@
 
 #include <iostream>
 #include "..\\Utility\\Utility.h"
-
+#define GET_ARRAY_LEN(arr, len){sizeof(arr) / sizeof(arr[0]);}
 
 using namespace std;
 using namespace Utility;
@@ -26,32 +26,47 @@ void main()
 	getchar();
 }
 
-int* MergeSorting(int *arr, int begin, int flag, int end){
-	
-	if((flag - begin) == 1 && (end - flag) == 1){
-		if (arr[begin] > arr[end]){
-			Exchange(arr[begin], arr[end]);
-		}
-		return arr;
-	} 
-	else if ((flag - begin) == 1 && (end - flag) ==0) {
-		return arr;
-	}
-	else{// definite position of the array memeber
-		int leftbegin = begin;
-		int leftflag = begin / 2;
-		int leftend = begin - leftflag;
-		int rightbegin = flag;
-		int rightflag = flag + (end - flag) / 2;
-		int rightend = end;
-		MergeSorting(arr, leftbegin, leftflag, leftend);
-		MergeSorting(arr, rightbegin, rightflag, rightend);
-	}
-}
+//int* MergeSorting(int *arr, int begin, int flag, int end){
+//	
+//	if((flag - begin) == 1 && (end - flag) == 0){
+//		if (arr[begin] > arr[end]){
+//			Exchange(arr[begin], arr[end]);
+//		}
+//		return arr;
+//	} 
+//	else if ((flag - begin) == 1 && (end - flag) ==0) {
+//		return arr;
+//	}
+//	else{// definite position of the array memeber
+//		int leftbegin = begin;
+//		int leftflag = begin / 2;
+//		int leftend = begin - leftflag;
+//		int rightbegin = flag + 1;
+//		int rightflag = flag + (end + 1 - flag) / 2;
+//		int rightend = end;
+//		MergeSorting(arr, leftbegin, leftflag, leftend);
+//		MergeSorting(arr, rightbegin, rightflag, rightend);
+//	}
+//}
+void MergSorting(int* arr, int begin, int flag, int end){
+	//flag = (end + begin) / 2
+	int LeftLen = flag - begin + 1;
+	int RightLen = end - flag;
+	int *LeftArray = new int[LeftLen];
+	int *RightArray = new int[RightLen];
+	copyArrayToAnother(arr, begin, flag, LeftArray);
+	copyArrayToAnother(arr, flag, end, RightArray);
 
+}
 void Exchange(int &a, int &b){
 	int tmp = 0;
 	tmp = a;
 	a = b;
 	b = tmp;
+}
+
+void copyArrayToAnother(int * SourceArray, int BeginPosition, int EndPostion, int* DestArray){
+	for (int i = 0; i < EndPostion + 1; i++){
+		DestArray[i] = SourceArray[i + BeginPosition];
+	}
 }
